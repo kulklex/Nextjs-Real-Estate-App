c488b9709e36dc5c8ed82c429c24590f1533a66d
kulklex <kulklex@gmail.com>
2023-02-19 12:32:11 +0100

    import React from 'react'

--- components/OutLet.jsx
+++ components/OutLet.jsx
@@ -1,3 +1,4 @@
+import React from 'react'
 import Head from 'next/head';
 import { Box } from '@chakra-ui/react';
 import Footer from './Footer';
--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,4 @@
+import React from 'react'
 import Image from "next/image"
 import Link from "next/link"
 import { baseUrl, fetchApi } from "../utils/fetchApi"
