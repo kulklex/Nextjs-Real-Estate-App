c6ed7d73a4087e4ab49f667eaaae2926a95fc330
kulklex <kulklex@gmail.com>
2023-02-19 19:02:33 +0100

    Added NProgress UI

--- pages/_app.js
+++ pages/_app.js
@@ -2,10 +2,22 @@ import Router from 'next/router';
 import Head from 'next/head';
 import { ChakraProvider } from '@chakra-ui/react';
 import OutLet from '../components/OutLet'
+import nProgress from 'nprogress';
 
 
 
 function MyApp({ Component, pageProps }) {
+  nProgress.configure({showSpinner: false})
+
+  Router.events.on('routeChangeStart', () => {
+    nProgress.start()
+  })
+
+  Router.events.on('routeChangeComplete', () => {
+    nProgress.done()
+  })
+
+  
   return (
     <>
       <Head>
